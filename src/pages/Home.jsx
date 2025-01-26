074f56388430d49bdb84c64ba435710883b73ca0
JustinPanCasetoMe <justinpanwithoutjustin195@gmail.com>
2025-01-26 21:10:09 +0800

    Home page animation of icon, mask, slogan, text building up Update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,14 @@
-import Origin from '../components/Origin'
-import FAQ from '../components/FAQ'
-import Contact from '../components/Contact'
 import Event_Content from '../components/Event_Content'
 import Indicator from '../components/Indicator';
 import Opening from '../components/Opening';
 
 const Home = () => {
   return (
-    <>
+    <div style={{marginTop:'100vh', paddingTop:"150px"}}>
       <Opening />
       <Indicator />
       <Event_Content />
-    </>
+    </div>
   )
 }
 
