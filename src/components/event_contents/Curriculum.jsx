590de4f0747f65eded8d8d03557b6e56cab70fed
JustinPanCasetoMe <justinpanwithoutjustin195@gmail.com>
2025-02-14 20:59:00 +0800

    Event Curriculum update

--- src/components/event_contents/Curriculum.jsx
+++ src/components/event_contents/Curriculum.jsx
@@ -4,6 +4,8 @@ const Curriculum = () => {
   return (
     <div className="mg-b-50">
       <h2 className="mg-b-30 topicTag">課表</h2>
+
+      {/* Chinese Lecture, Chinese version curriculum */}
       <table
         style={{
           borderCollapse: "collapse",
@@ -14,46 +16,131 @@ const Curriculum = () => {
       >
         <thead>
           <tr style={{backgroundColor: "#E57D3D", color: "white"}}>
+            <th>Time</th>
             <th>D1</th>
-            <th colSpan={2}>D2</th>
+            <th>D2</th>
             <th>D3</th>
           </tr>
         </thead>
         <tbody>
           <tr>
-            <td>開幕式</td>
-            <td rowSpan={3}>自走車設計實作</td>
-            <td>類比電路應用實作</td>
-            <td rowSpan={2}>半導體製程概論</td>
+            <td></td>
+            <td></td>
+            <td>早餐</td>
+            <td>早餐</td>
           </tr>
           <tr>
-            <td>進階邏輯設計</td>
-            <td rowSpan={2}>電路板設計實作</td>
+            <td>9:30</td>
+            <td>報到</td>
+            <td rowSpan={2}>製程概論</td>
+            <td>前往博愛</td>
           </tr>
           <tr>
-            <td>交流與對談</td>
-            <td>自走車最佳化</td>
+            <td>10:00</td>
+            <td>開幕+沙龍座談</td>
+            <td>成果報告or比賽</td>
           </tr>
           <tr>
-            <td rowSpan={3}>電子電路與 IC 設計概論</td>
-            <td rowSpan={1}>類比電路應用實作</td>
-            <td rowSpan={3}>自走車設計實作</td>
-            <td rowSpan={2}>自走車競賽</td>
+            <td>12:00</td>
+            <td>午餐</td>
+            <td>午餐</td>
+            <td>午餐</td>
           </tr>
           <tr>
-            <td rowSpan={2}>電路板設計實作</td>
+            <td>13:30</td>
+            <td>半導體產業+晶片概論</td>
+            <td rowSpan={2}>晶片設計實驗</td>
+            <td>總經理演講</td>
           </tr>
           <tr>
-            <td>新思總經理演講</td>
+            <td>14:30</td>
+            <td>晶片設計理論</td>
+            <td>閉幕式</td>
           </tr>
           <tr>
-            <td>產業圓桌分享講座</td>
-            <td colSpan={2}>在學生分享講座</td>
-            <td>結業與頒獎儀式</td>
+            <td>17:30</td>
+            <td>晚餐</td>
+            <td>晚餐</td>
+            <td></td>
           </tr>
+          <tr>
+            <td>18:30</td>
+            <td>交流活動or寓教於樂的遊戲</td>
+            <td>晶片設計專案討論or競賽準備</td>
+            <td></td>
+          </tr>
+        </tbody>
+      </table>
+
+      {/* English Lecture, Chinese version curriculum */}
+      <table
+        style={{
+          borderCollapse: "collapse",
+          width: "100%",
+          border: "none",
+          textAlign:"center"
+        }}
+      >
+        <thead>
+          <tr style={{backgroundColor: "#E57D3D", color: "white"}}>
+            <th>Time</th>
+            <th>D1</th>
+            <th>D2</th>
+            <th>D3</th>
+            <th>D4</th>
+          </tr>
+        </thead>
+        <tbody>
           <tr>
             <td></td>
-            <td colSpan={2}>自走車最佳化</td>
+            <td></td>
+            <td>早餐</td>
+            <td>早餐</td>
+            <td>早餐</td>
+          </tr>
+          <tr>
+            <td>9:30</td>
+            <td>報到</td>
+            <td rowSpan={2}>製程概論</td>
+            <td rowSpan={2}>晶片設計實驗</td>
+            <td>前往博愛</td>
+          </tr>
+          <tr>
+            <td>10:00</td>
+            <td>開幕+沙龍座談</td>
+            <td>成果報告or比賽</td>
+          </tr>
+          <tr>
+            <td>12:00</td>
+            <td>午餐</td>
+            <td>午餐</td>
+            <td>午餐</td>
+            <td>午餐</td>
+          </tr>
+          <tr>
+            <td>13:30</td>
+            <td>半導體產業+晶片概論</td>
+            <td rowSpan={2}>參訪TSMC, TASA</td>
+            <td rowSpan={2}>交流-與新思高管</td>
+            <td>總經理演講</td>
+          </tr>
+          <tr>
+            <td>14:30</td>
+            <td>晶片設計理論</td>
+            <td>閉幕式</td>
+          </tr>
+          <tr>
+            <td>17:30</td>
+            <td>晚餐</td>
+            <td>晚餐</td>
+            <td>晚餐</td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>18:30</td>
+            <td>寓教於樂的遊戲</td>
+            <td>交流活動</td>
+            <td>晶片設計專案討論or競賽準備</td>
             <td></td>
           </tr>
         </tbody>
