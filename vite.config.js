ad7a0770372c814bd02b44ce1327a24624a11e30
JustinPanCasetoMe <justinpanwithoutjustin195@gmail.com>
2025-03-17 01:35:01 +0800

    vite.config.js text Update

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'/'
+  base:'/',
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    // setupFiles: './src/test/setup.js',
+    include: ['**/*.{test,spec}.{js,ts,jsx,tsx}'],
+    exclude: ['**/node_modules/**', '**/dist/**'],
+  },
 })
