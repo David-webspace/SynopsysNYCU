e5a5084f25b6b7037a81a1c7e2d3ea1c35e51bb1
JustinPanCasetoMe <justinpanwithoutjustin195@gmail.com>
2025-04-01 18:36:21 +0800

    Adjust error path

--- src/pages/Event_Review.jsx
+++ src/pages/Event_Review.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import Reflection from '../components/event_review/Reflection/'
-import Review_Highlight from '../components/event_review/Review_Highlight/'
+import Reflection from '../components/event_review/Reflection'
+import Review_Highlight from '../components/event_review/Review_Highlight'
 
 const Event_Review = () => {
   return (
