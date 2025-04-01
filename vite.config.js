bef45c82dc66c1e2831312167148926b6978db8c
JustinPanCasetoMe <justinpanwithoutjustin195@gmail.com>
2025-04-01 15:30:45 +0800

    Deploy

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base:'/',
-  base: mode === 'development' ? '/dev/' : '/', // Use '/dev/' for development deployment
+  base:'/',
   test: {
     globals: true,
     environment: 'jsdom',
