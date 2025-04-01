53111a36849357a1d00cc032949060ac578af300
JustinPanCasetoMe <justinpanwithoutjustin195@gmail.com>
2025-04-01 20:42:46 +0800

    Devpath

--- src/App.jsx
+++ src/App.jsx
@@ -27,17 +27,17 @@ function App() {
         <Route path='/' element={<Home />} />
         <Route path='/dev' element={<Homedev />} />
 
-        <Route path='dev/event2025' element={<Event_2025 />} />
-        <Route path='dev/event-review' element={<Event_Review />} />
-        <Route path='dev/online-resource' element={<Online_Free_Resource />} />
+        <Route path='/dev/event2025' element={<Event_2025 />} />
+        <Route path='/dev/event-review' element={<Event_Review />} />
+        <Route path='/dev/online-resource' element={<Online_Free_Resource />} />
 
-        <Route path='dev/origin' element={<Origin />} />
-        <Route path='dev/faqs' element={<FAQ />} />
-        <Route path='dev/contacts' element={<Contact />} />
+        <Route path='/dev/origin' element={<Origin />} />
+        <Route path='/dev/faqs' element={<FAQ />} />
+        <Route path='/dev/contacts' element={<Contact />} />
 
 
-        <Route path='dev/backend' element={<Backend />}/>
-        <Route path='dev/visit' element={<Backend />}/>
+        <Route path='/dev/backend' element={<Backend />}/>
+        <Route path='/dev/visit' element={<Backend />}/>
 
       </Routes>
 
