8649361a6f2b2009eeaf2390ab74cfe9102841f8
JustinPanCasetoMe <justinpanwithoutjustin195@gmail.com>
2025-04-07 21:41:11 +0800

    Contact Media Link

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -3,13 +3,18 @@ import { Link } from 'react-router-dom'
 import '../i18n'
 import { useTranslation } from 'react-i18next'
 import { FaFacebook } from "react-icons/fa";
+import { IoLocationSharp, IoLogoInstagram, IoLogoFacebook, IoLogoYoutube } from "react-icons/io5";
+import '../css/contact.css'
 
 const Contacts = () => {
 
   const { t, i18n } = useTranslation()
 
   const ScreenWidth = window.innerWidth
-
+  const locationLink = 'https://maps.app.goo.gl/YuKrLBgWdcw2Y8L69'
+  const instagramLink = 'https://www.instagram.com/sawago88/'
+  const facebookLink = 'https://www.facebook.com/NYCU.SYNOPSYS.SEMICONDUCTOR.ICDESIGN/'
+  const youtubeLink = 'https://www.youtube.com/@nycu_synopsys_semi'
 
   return (
     <div className='Container'>
@@ -24,40 +29,48 @@ const Contacts = () => {
       </div>
 
       <>
-        <div className={`${(1024<=ScreenWidth && ScreenWidth < 1960) ? '' : 'dn'}`}>
+        {/* <div className={`${(1024<=ScreenWidth && ScreenWidth < 1960) ? '' : 'dn'}`}>
           <ul className='df'>
             <a href='https://maps.app.goo.gl/YuKrLBgWdcw2Y8L69' className='outerLink mg-r-30' target='_blank'><li>{t('地址')}</li></a>
             <a href='https://www.youtube.com/@nycu_synopsys_semi' className='outerLink mg-r-30' target='_blank'><li>{t('線上課程')}</li></a>
             <a href='https://www.facebook.com/NYCU.SYNOPSYS.SEMICONDUCTOR.ICDESIGN/' className='outerLink mg-r-30' target='_blank'><li>{t('臉書')}</li></a>
           </ul>
+        </div> */}
+        <div className={`${(1024<=ScreenWidth && ScreenWidth < 1960) ? '' : 'dn'}`}>
+          <ul className='df'>
+            <a href={locationLink} className='mediaLink' target='_blank'><li><IoLocationSharp size={20}/></li></a>
+            <a href={instagramLink} className='mediaLink' target='_blank'><li><IoLogoInstagram size={20}/></li></a>
+            <a href={facebookLink} className='mediaLink' target='_blank'><li><IoLogoFacebook size={20}/></li></a>
+            <a href={youtubeLink} className='mediaLink' target='_blank'><li><IoLogoYoutube size={20}/></li></a>
+          </ul>
         </div>
 
-        <div className={`${(768<=ScreenWidth && ScreenWidth < 1024) ? '' : 'dn'}`}>
+        {/* <div className={`${(768<=ScreenWidth && ScreenWidth < 1024) ? '' : 'dn'}`}>
           <ul className='df txt-aln-c'>
             <a href='https://maps.app.goo.gl/bR5LAyeQiwJNsrxx8' style={{width:'100%'}} className='outerLink mg-r-30 mg-b-20 db' target='_blank'><li>地址</li></a>
             <a href='https://www.instagram.com/sawago88/' style={{width:'100%'}} className='outerLink mg-r-30 mg-b-20 db' target='_blank'><li>IG</li></a>
             <a href='https://www.facebook.com/chc888' style={{width:'100%'}} className='outerLink mg-r-30 mg-b-20 db' target='_blank'><li>FB</li></a>
             <a href='https://www.youtube.com/@nycu_synopsys_semi/videos' style={{width:'100%'}} className='outerLink mg-r-30 mg-b-20 db' target='_blank'><li>紀錄片</li></a>
           </ul>
-        </div>
+        </div> */}
 
-        <div className={`${(425<=ScreenWidth && ScreenWidth < 768) ? '' : 'dn'}`}>
+        {/* <div className={`${(425<=ScreenWidth && ScreenWidth < 768) ? '' : 'dn'}`}>
           <ul className='txt-aln-c'>
             <a href='https://maps.app.goo.gl/bR5LAyeQiwJNsrxx8' style={{width:'100%'}} className='outerLink mg-b-20 db' target='_blank'><li>地址</li></a>
             <a href='https://www.instagram.com/sawago88/' style={{width:'100%'}} className='outerLink mg-b-20 db' target='_blank'><li>IG</li></a>
             <a href='https://www.facebook.com/chc888' style={{width:'100%'}} className='outerLink mg-b-20 db' target='_blank'><li>FB</li></a>
             <a href='https://www.youtube.com/@nycu_synopsys_semi/videos' style={{width:'100%'}} className='outerLink mg-b-20 db' target='_blank'><li>紀錄片</li></a>
           </ul>
-        </div>
+        </div> */}
 
-        <div className={`${(375<=ScreenWidth && ScreenWidth < 425) ? '' : 'dn'}`}>
+        {/* <div className={`${(375<=ScreenWidth && ScreenWidth < 425) ? '' : 'dn'}`}>
           <ul className='txt-aln-c'>
             <a href='https://maps.app.goo.gl/bR5LAyeQiwJNsrxx8' style={{width:'100%'}} className='outerLink mg-b-20 db' target='_blank'><li>地址</li></a>
             <a href='https://www.instagram.com/sawago88/' style={{width:'100%'}} className='outerLink mg-b-20 db' target='_blank'><li>IG</li></a>
             <a href='https://www.facebook.com/chc888' style={{width:'100%'}} className='outerLink mg-b-20 db' target='_blank'><li>FB</li></a>
             <a href='https://www.youtube.com/@nycu_synopsys_semi/videos' style={{width:'100%'}} className='outerLink mg-b-20 db' target='_blank'><li>紀錄片</li></a>
           </ul>
-        </div>
+        </div> */}
       </>
 
     </div>
